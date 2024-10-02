d4fb1c0c04097f6d28bf3f41d03084c2e5267399
Dary van Sleeuwen <d.vansleeuwen@lameco.nl>
2024-10-02 10:13:06 +0200

    Register template root for to scope searching component in this plugin. Button component setup

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,8 +4,13 @@ namespace lameco\crafttwigcomponents;
 
 use Craft;
 use craft\base\Plugin as BasePlugin;
+use craft\services\Plugins;
 use craft\web\View;
 use lameco\crafttwigcomponents\AssetsBundles\StimulusBundle;
+use modules\lameco\web\twig\WebtesterExtension;
+use Performing\TwigComponents\Configuration;
+use TalesFromADev\Twig\Extra\Tailwind\TailwindExtension;
+use Twig\Extra\Html\HtmlExtension;
 use yii\base\Event;
 use yii\base\InvalidConfigException;
 
@@ -28,19 +33,34 @@ class Plugin extends BasePlugin
     public function init(): void
     {
         parent::init();
-
-        //Register custom template root so you can use "@lameco" alias for referencing to components in this plugin
-        Event::on(
-            View::class,
-            View::EVENT_REGISTER_SITE_TEMPLATE_ROOTS,
-            function ($event) {
-                $event->roots['@lameco'] = $this->getBasePath() . '/templates';
-            }
-        );
+        $twig = Craft::$app->getView()->getTwig();
 
         if (Craft::$app->request->getIsSiteRequest()) {
             //Register AssetBundle for stimulus controller so that Craft auto-injects them on the website
             Craft::$app->view->registerAssetBundle(StimulusBundle::class);
+        }
+
+        Craft::$app->view->registerTwigExtension(new HtmlExtension());
+
+        Event::on(
+            View::class,
+            View::EVENT_REGISTER_SITE_TEMPLATE_ROOTS,
+            function ($event) {
+                $event->roots['lameco'] = $this->getBasePath() . '/templates/components';
+            }
+        );
+
+        if (Craft::$app->request->getIsSiteRequest()) {
+            Event::on(
+                Plugins::class,
+                Plugins::EVENT_AFTER_LOAD_PLUGINS,
+                function (Event $event) use ($twig) {
+                    Configuration::make($twig)
+                        ->setTemplatesPath('lameco')
+                        ->useCustomTags()
+                        ->setup();
+                }
+            );
         }
     }
 }
