f791236038b6af26567158ca54ae81d72de79454
Sander van de Vondervoort <s.vdvondervoort@lameco.nl>
2025-08-07 15:26:22 +0200

    Added FontAwesome.ts with export of PreDefined icons

---
+++ src/assets/ts/fontAwesome.ts
@@ -0,0 +1,28 @@
+"use strict";
+
+import { faFacebookF as fabFacebook } from "@fortawesome/free-brands-svg-icons/faFacebookF";
+import { faInstagram as fabInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
+import { faYoutube as fabYoutube } from "@fortawesome/free-brands-svg-icons/faYoutube";
+import { faXTwitter as fabTwitter } from "@fortawesome/free-brands-svg-icons/faXTwitter";
+import { faTiktok as fabTiktok } from "@fortawesome/free-brands-svg-icons/faTiktok";
+import { faLinkedinIn as fabLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedinIn";
+
+import { faEnvelope as farEnvelope } from "@fortawesome/pro-regular-svg-icons/faEnvelope";
+import { faAngleLeft as farAngleLeft } from "@fortawesome/pro-regular-svg-icons/faAngleLeft";
+import { faAngleDown as farAngleDown } from "@fortawesome/pro-regular-svg-icons/faAngleDown";
+
+/**
+ * FontAwesome brand icons used in Twig Components.
+ */
+export const twigComponentsFontAwesomeIcons = [
+  fabFacebook,
+  fabInstagram,
+  fabYoutube,
+  fabTwitter,
+  fabTiktok,
+  fabLinkedin,
+
+  farEnvelope,
+  farAngleLeft,
+  farAngleDown,
+];
